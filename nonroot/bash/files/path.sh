532a5ab463aba54b04508c8eebf4f143ec4d50e3
RyanHir <4690732+RyanHir@users.noreply.github.com>
2019-08-21 15:28:17 -0500

    java home

--- nonroot/bash/files/path.sh
+++ nonroot/bash/files/path.sh
@@ -2,3 +2,5 @@ export PATH="$PATH:/home/ryan/.local/bin/"
 export PATH="$PATH:/home/ryan/.bin/flutter/bin/"
 export PATH="$PATH:/home/ryan/.bin/flutter/bin/cache/dart-sdk/bin/"
 export PATH="$PATH":"$HOME/.bin/flutter/.pub-cache/bin"
+
+export JAVA_HOME="/usr/lib/jvm/java-11-openjdk/"
