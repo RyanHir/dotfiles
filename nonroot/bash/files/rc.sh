b224bfcb15d1e624076bdb9bb4c5d45da202a050
RyanHir <4690732+RyanHir@users.noreply.github.com>
2019-08-26 13:52:25 -0500

    use correct tty

--- nonroot/bash/files/rc.sh
+++ nonroot/bash/files/rc.sh
@@ -1,6 +1,6 @@
 # /etc/bash/bashrc
 
-if [[ -z $DISPLAY ]] && [[ $(tty) = /dev/tty3 ]]; then exec startx; fi
+if [[ -z $DISPLAY ]] && [[ $(tty) = /dev/tty1 ]]; then exec startx; fi
 
 shopt -s checkwinsize
 
