a446647137beb9ed68390943784021a052ea7341
Ryan Hirasaki <ryanhirasaki@gmail.com>
2020-11-06 11:24:45 -0600

    Update install script

--- install.sh
+++ install.sh
@@ -1,54 +1,33 @@
 #! /bin/bash
 
 function usage {
-	echo -e "./$(basename "$0") -h: Shows Help Message"
-	echo -e "./$(basename "$0") -p: Do Install Packages"
-	echo -e "./$(basename "$0") -y: Auto Accept Dialogs"
+	EXEC_PATH=$(dirname "$0")
+	EXEC_NAME=$(basename "$0")
+	echo -e "$EXEC_PATH/$EXEC_NAME -h: Shows Help Message"
 }
-
-export INSTALLPKG=false
-export AUTOACCEPT=""
 
 while getopts ":h:py" o; do
 	case "${o}" in
 		h) usage; exit;;
-		p) export INSTALLPKG=true;;
-		y) export AUTOACCEPT="-y";;
 		?) echo "Invalid Option: -$OPTARG"; usage; exit 2;;
 	esac
 done
 
-{
-	cd src || exit 3
+cd src || exit $?
 	cp -r . "$HOME/"
-}
+cd - > /dev/null || exit $?
 
-PACKAGES=(zsh git fonts-noto)
-
-if $INSTALLPKG
+if [ -r "/etc/passwd" ]
 then
-	if ! [ "$UID" = "0" ]
-	then
-		if ! command -v sudo 1> /dev/null
-		then
-			echo "Not ran as root"
-			exit
-		else
-			SUDO=sudo
-		fi
-	fi
-	if command -v apt-get 1> /dev/null
-	then
-		$SUDO apt-get update $AUTOACCEPT -qq
-		$SUDO apt-get install $AUTOACCEPT -qq "${PACKAGES[@]}" || exit 4
-	fi
-fi
+	ZSH_PATH=$(command -v zsh || exit $?)
+	DEFAULT_SHELL=$(awk -F: "/^$USER/{print \$7}" /etc/passwd)
 
-DEFAULT_SHELL=$(grep "^$USER:" /etc/passwd | sed 's/.*://;s/.*\///')
-
-if [ "$DEFAULT_SHELL" != "zsh" ]
-then
-	LOC=$(command -v zsh || exit 5)
-	echo "Password Required to change shell"
-	chsh -s "$LOC"
+	if [ "$DEFAULT_SHELL" != "$ZSH_PATH" ]
+	then
+		echo "Password Required to change shell"
+		chsh -s "$ZSH_PATH"
+	fi
+else
+	echo "Cannot open /etc/passwd. Required to get default shell!"
+	echo "User must change shell on their own"
 fi
