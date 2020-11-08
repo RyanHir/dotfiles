e0277669059082dec20dcc1d8bc5f3630f90a7b9
Ryan Hirasaki <ryanhirasaki@gmail.com>
2020-11-08 01:32:16 -0600

    Better Audio Sink Selection

--- src/.config/i3/vol.sh
+++ src/.config/i3/vol.sh
@@ -1,7 +1,8 @@
 #! /bin/sh
 
 operator=$(echo "$1" | cut -c1-1)
-current=$(pacmd dump-volumes | awk 'NR==1{print $8}' | sed 's/\%//')
-[ $operator = "+" ] && [ $current -ge 100 ] && exit
+current=$(sh $HOME/.local/bin/statusbar/volume | sed "s/.*://;s/%//" | tr -d " ")
+
+[ "$operator" = "+" ] && [ "$current" -ge 100 ] && exit
 
 pactl set-sink-volume @DEFAULT_SINK@ $1
