45a38fa1de5b2b65735ba850613fd8113b859078
Ryan Hirasaki <ryanhirasaki@gmail.com>
2020-11-08 01:35:41 -0600

    Max volume fix

--- src/.config/i3/vol.sh
+++ src/.config/i3/vol.sh
@@ -3,6 +3,12 @@
 operator=$(echo "$1" | cut -c1-1)
 current=$(sh $HOME/.local/bin/statusbar/volume | sed "s/.*://;s/%//" | tr -d " ")
 
-[ "$operator" = "+" ] && [ "$current" -ge 100 ] && exit
+if [ "$operator" = "+" ]
+then
+	if [ "$current" -ge 100 ]
+	then
+		exit
+	fi
+fi
 
 pactl set-sink-volume @DEFAULT_SINK@ $1
