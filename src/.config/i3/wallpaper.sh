c32fd7c3bd4b5d48446d671840d213471802c71c
Ryan Hirasaki <ryanhirasaki@gmail.com>
2020-11-12 20:55:57 -0600

    set wallpaper tool

--- src/.config/i3/wallpaper.sh
+++ src/.config/i3/wallpaper.sh
@@ -2,11 +2,12 @@
 
 set -e
 
-SLEEP_MINUTES="$1"
+. ~/.config/env
 
+SLEEP_MINUTES="$1"
 SLEEP_SECONDS=$((SLEEP_MINUTES * 60))
 while true; do
-	feh --bg-fill ~/.cache/i3lock/current/wall.png
+	set-wallpaper
 	echo "Sleeping for $SLEEP_MINUTES minute(s)"
 	sleep "$SLEEP_SECONDS"
 done
