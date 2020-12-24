8d61d2cdb1744f5247ef658dae5c7c9792a6b706
Ryan Hirasaki <ryanhirasaki@gmail.com>
2020-12-23 21:38:17 -0600

    Improve display hotkey

--- src/.config/i3/display.sh
+++ src/.config/i3/display.sh
@@ -1,3 +1,11 @@
 #! /bin/sh
 
-xrandr --output eDP-1 --auto --output DP-2-1 --auto --left-of eDP-1 --primary
+# Disable Displays that are disconnected but think they still exist
+xrandr | awk '/disconnected/{print $1}' | xargs -I{} xrandr --output {} --off
+
+# Laptop Display
+xrandr --output eDP-1 --auto
+
+if xrandr | grep "DP-2-1 connected" > /dev/null; then
+	xrandr --output DP-2-1 --auto --left-of eDP-1 --primary
+fi
