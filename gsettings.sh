276aee37ebc69f0b6216e7b5168a0577cf5be1ad
Ryan Hirasaki <ryanhirasaki@gmail.com>
2021-02-03 15:22:25 -0600

    Comments

--- gsettings.sh
+++ gsettings.sh
@@ -6,12 +6,16 @@ dconf reset -f /org/gnome/
 
 GSET="gsettings set"
 
+# Theme
 $GSET org.gnome.desktop.interface cursor-theme 'Adwaita'
 $GSET org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
 $GSET org.gnome.desktop.interface icon-theme 'Adwaita'
+
+# Touchpad
 $GSET org.gnome.desktop.peripherals.touchpad natural-scroll true
 $GSET org.gnome.desktop.peripherals.touchpad tap-to-click true
 $GSET org.gnome.desktop.wm.preferences button-layout ':close'
 
+# Dash layout
 $GSET org.gnome.shell favorite-apps "['firefox.desktop', 'spotify.desktop', 'org.gnome.Terminal.desktop', 'org.gnome.Nautilus.desktop']"
 
