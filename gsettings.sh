79d6ae91a1cab65e31e4ee243d6b9348ffa4b413
Ryan Hirasaki <ryanhirasaki@gmail.com>
2021-02-03 15:10:58 -0600

    misc

--- gsettings.sh
+++ gsettings.sh
@@ -2,22 +2,17 @@
 
 command -v gsettings > /dev/null || exit
 
+dconf reset -f /org/gnome/
+
 GSET="gsettings set"
 
-KEYBIND="/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/"
-
-$GSET org.gnome.desktop.peripherals.touchpad tap-to-click true
+$GSET org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
 $GSET org.gnome.desktop.peripherals.touchpad natural-scroll true
+$GSET org.gnome.desktop.peripherals.touchpad tap-to-click true
 $GSET org.gnome.desktop.wm.preferences button-layout ':close'
 
-$GSET org.gnome.settings-daemon.plugins.media-keys custom-keybindings \
-    "['$KEYBIND/custom0/']"
-
-dconf reset -f "$KEYBIND" 
-dconf load "$KEYBIND" << EOF
-[custom0]
-binding='<Primary><Alt>t'
-command='gnome-terminal'
-name='term'
+dconf load /org/gnome/shell/ << EOF
+[/]
+favorite-apps=['firefox.desktop', 'spotify.desktop', 'org.gnome.Terminal.desktop', 'org.gnome.Nautilus.desktop']
 EOF
 
