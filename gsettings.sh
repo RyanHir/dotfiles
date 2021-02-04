3cd54d2ae9de9a7d2164695bb52ed223bc7d7b35
Ryan Hirasaki <ryanhirasaki@gmail.com>
2021-02-04 10:41:14 -0600

    Windows like Alt-Tab

--- gsettings.sh
+++ gsettings.sh
@@ -17,6 +17,13 @@ $GSET org.gnome.desktop.peripherals.touchpad natural-scroll true
 $GSET org.gnome.desktop.peripherals.touchpad tap-to-click true
 $GSET org.gnome.desktop.wm.preferences button-layout ':close'
 
+# Shortcuts
+$GSET org.gnome.desktop.wm.keybindings switch-applications "[]"
+$GSET org.gnome.desktop.wm.keybindings switch-applications-backward "[]"
+$GSET org.gnome.desktop.wm.keybindings switch-windows "['<alt>tab']"
+$GSET org.gnome.desktop.wm.keybindings switch-windows-backward \
+    "['<Shift><Alt>Tab']"
+
 # Dash layout
 $GSET org.gnome.shell favorite-apps "['firefox.desktop', 'spotify.desktop', 'org.gnome.Terminal.desktop', 'org.gnome.Nautilus.desktop']"
 
