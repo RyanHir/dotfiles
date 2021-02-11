3347c6e771dca5094f563ba7e5b5022aa10d7d92
Ryan Hirasaki <ryanhirasaki@gmail.com>
2021-02-11 15:31:56 -0600

    Cleanup

--- gsettings.sh
+++ gsettings.sh
@@ -1,29 +1,42 @@
-#! /bin/sh
+#! /usr/bin/env bash
 
 # VARS
 GSET="gsettings set"
+GDESKTOP="org.gnome.desktop"
+GINTERFACE="$GDESKTOP.interface"
+GKEYBIND="$GDESKTOP.wm.keybindings"
 
 # Clean slate
 dconf reset -f /org/gnome/
 
 # Theme
-$GSET org.gnome.desktop.interface clock-format '12h'
-$GSET org.gnome.desktop.interface cursor-theme 'Adwaita'
-$GSET org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
-$GSET org.gnome.desktop.interface icon-theme 'Adwaita'
+$GSET $GINTERFACE clock-format '12h'
+$GSET $GINTERFACE cursor-theme 'Adwaita'
+$GSET $GINTERFACE gtk-theme 'Adwaita-dark'
+$GSET $GINTERFACE icon-theme 'Adwaita'
 
 # Touchpad
-$GSET org.gnome.desktop.peripherals.touchpad natural-scroll true
-$GSET org.gnome.desktop.peripherals.touchpad tap-to-click true
-$GSET org.gnome.desktop.wm.preferences button-layout ':close'
+$GSET $GDESKTOP.peripherals.touchpad natural-scroll true
+$GSET $GDESKTOP.peripherals.touchpad tap-to-click true
+$GSET $GDESKTOP.wm.preferences button-layout ':close'
 
 # Shortcuts
-$GSET org.gnome.desktop.wm.keybindings switch-applications "[]"
-$GSET org.gnome.desktop.wm.keybindings switch-applications-backward "[]"
-$GSET org.gnome.desktop.wm.keybindings switch-windows "['<alt>tab']"
-$GSET org.gnome.desktop.wm.keybindings switch-windows-backward \
-    "['<Shift><Alt>Tab']"
+$GSET $GKEYBIND switch-applications "[]"
+$GSET $GKEYBIND switch-applications-backward "[]"
+$GSET $GKEYBIND switch-windows "['<alt>tab']"
+$GSET $GKEYBIND switch-windows-backward "['<Shift><Alt>Tab']"
 
 # Dash layout
 $GSET org.gnome.shell favorite-apps "['firefox.desktop', 'spotify.desktop', 'org.gnome.Terminal.desktop', 'org.gnome.Nautilus.desktop']"
+
+# Terminal
+term_profile=$(gsettings get org.gnome.Terminal.ProfilesList default)
+term_profile=${term_profile:1:-1}
+term_dconf="/org/gnome/terminal/legacy/profiles:/:$term_profile/"
+term_gsetting="org.gnome.Terminal.Legacy.Profile:$term_dconf"
+
+$GSET "$term_gsetting" use-theme-colors false
+$GSET "$term_gsetting" bold-is-bright true
+$GSET "$term_gsetting" background-color 'rgb(46,52,54)'
+$GSET "$term_gsetting" foreground-color 'rgb(211,215,207)'
 
