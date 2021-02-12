ee5689c5d551f19751c1738d98a8964680cfd3ce
Ryan Hirasaki <ryanhirasaki@gmail.com>
2021-02-11 18:55:27 -0600

    Keyboard shortcut

--- gsettings.sh
+++ gsettings.sh
@@ -5,6 +5,7 @@ GSET="gsettings set"
 GDESKTOP="org.gnome.desktop"
 GINTERFACE="$GDESKTOP.interface"
 GKEYBIND="$GDESKTOP.wm.keybindings"
+GDAEMON="org.gnome.settings-daemon.plugins.media-keys"
 
 # Clean slate
 dconf reset -f /org/gnome/
@@ -25,6 +26,15 @@ $GSET $GKEYBIND switch-applications "[]"
 $GSET $GKEYBIND switch-applications-backward "[]"
 $GSET $GKEYBIND switch-windows "['<alt>tab']"
 $GSET $GKEYBIND switch-windows-backward "['<Shift><Alt>Tab']"
+
+DDAEMON_C0="/${GDAEMON//\./\/}/custom-keybindings/custom0/" 
+$GSET $GDAEMON custom-keybindings "['$DDAEMON_C0']"
+dconf load "$DDAEMON_C0" << EOF
+[/]
+binding='<Primary><Alt>t'
+command='gnome-terminal'
+name='term'
+EOF
 
 # Dash layout
 $GSET org.gnome.shell favorite-apps "['firefox.desktop', 'spotify.desktop', 'org.gnome.Terminal.desktop', 'org.gnome.Nautilus.desktop']"
