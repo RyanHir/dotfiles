bc306dbcafdb9d0fd27a1a1d5c8c39ac7d21d928
Ryan Hirasaki <4690732+RyanHir@users.noreply.github.com>
2021-12-24 19:35:02 -0600

    Revive dotfiles for KDE/ArchLinux system

--- src/.config/i3/lock.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env bash
-
-. "$HOME/.config/wallpaper.env"
-
-#SHAPE=$(identify "$WALL_PATH" | awk '{print $4}')
-#convert "$WALL_PATH" RGB:- \
-#    | i3lock --raw "$SHAPE:rgb" --image /dev/stdin
-
-i3lock -c "$WALL_COLR"
